fd8219bd6ea45719de99c9447280eadaff28f7e7
yeseong31 <yeseong31@naver.com>
2023-02-05 21:15:49 +0900

    feat: eventlet 통신을 위한 wsgi 설정 추가

--- config/wsgi.py
+++ config/wsgi.py
@@ -9,8 +9,15 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 
 import os
 
+import eventlet.wsgi
 from django.core.wsgi import get_wsgi_application
+from socketio import WSGIApp
+
+from chat.views.sio_views import sio
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
-application = get_wsgi_application()
+django_app = get_wsgi_application()
+application = WSGIApp(sio, django_app)
+
+eventlet.wsgi.server(eventlet.listen(('', 8000)), application)
