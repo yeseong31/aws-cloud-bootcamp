904f2b8b6ee1ffec5c158276a5adcee23a6e99d3
yeseong31 <yeseong31@naver.com>
2023-02-07 10:32:56 +0900

    fix: log 경로 인식 문제 수정

--- config/settings/base.py
+++ config/settings/base.py
@@ -154,7 +154,7 @@ LOGGING = {
             'level': 'INFO',
             'filters': ['require_debug_false'],
             'class': 'logging.handlers.RotatingFileHandler',
-            'filename': BASE_DIR / 'logs/awsbootcamp.log',
+            'filename': f'{BASE_DIR}/logs/awsbootcamp.log',
             'maxBytes': 1024 * 1024 * 5,  # 5 MB
             'backupCount': 5,
             'formatter': 'standard',
