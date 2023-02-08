e1aa6e26fec6d939abc18f0ef4d4f946d64eb7be
yeseong31 <yeseong31@naver.com>
2023-02-08 09:57:00 +0900

    feat: Room model 작성

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Room(models.Model):
+    id = models.IntegerField(primary_key=True, verbose_name='방 ID')
+    name = models.CharField(verbose_name='방 이름')
+    description = models.TextField(verbose_name='방 설명')
+    owner = models.CharField(max_length=128, verbose_name='방장 이름')
+    current = models.IntegerField(default=0, verbose_name='현재 참여 인원 수')
+    total = models.IntegerField(default=30, verbose_name='최대 참여 인원 수')
+    created_at = models.DateTimeField(verbose_name='생성일', default=timezone.now())
+    modified_at = models.DateTimeField(verbose_name='수정일', null=True, blank=True)
