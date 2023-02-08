34fb29337e6826375d5812ab4112d93b9ef68770
yeseong31 <yeseong31@naver.com>
2023-02-08 10:47:39 +0900

    chore: create_at default 값 설정 변경
    timezone.now()에서timezone.now로 변경

--- chat/models.py
+++ chat/models.py
@@ -9,5 +9,5 @@ class Room(models.Model):
     owner = models.CharField(max_length=128, verbose_name='방장 이름')
     current = models.IntegerField(default=0, verbose_name='현재 참여 인원 수')
     total = models.IntegerField(default=30, verbose_name='최대 참여 인원 수')
-    created_at = models.DateTimeField(verbose_name='생성일', default=timezone.now())
+    created_at = models.DateTimeField(verbose_name='생성일', default=timezone.now)
     modified_at = models.DateTimeField(verbose_name='수정일', null=True, blank=True)
