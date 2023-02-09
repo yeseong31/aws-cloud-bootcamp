07d62f343206491092bb070f05615fc2fea8f58a
yeseong31 <yeseong31@naver.com>
2023-02-09 16:37:31 +0900

    feat: 전체 방 목록 조회

--- chat/views/base_views.py
+++ chat/views/base_views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 
+from chat.models import Room
+
 
 def home(request):
-    return render(request, 'home.html')
+    """
+    전체 방 목록 조회
+    :return:
+    - room_list: 방 목록
+    """
+    room_list = Room.objects.all().order_by('-created_at')
+    context = {'room_list': room_list}
+    return render(request, 'home.html', context)
