f720deab5f9558e7808519930ab5afa29435191a
yeseong31 <yeseong31@naver.com>
2023-02-09 15:42:57 +0900

    chore: url 수정

--- chat/urls.py
+++ chat/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 
-from chat.views import base_views, sio_views
+from chat.views import base_views
 
 app_name = 'chat'
 
 urlpatterns = [
-    # ----- Homepage -----
+    # 홈페이지
     path('', base_views.home, name='home'),
-    # ----- SocketIO -----
-    path('chat/', sio_views.chat, name='chat'),
+    # 방 생성
+    path('room/create/', base_views.create_room, name='create_room'),
+    # 방 입장 (SocketIO)
+    path('chat/<str:room_code>/', base_views.chat, name='chat'),
 ]
