10782fd9eca359373b68202ebaa8b2b1ef67e35a
yeseong31 <yeseong31@naver.com>
2023-03-02 12:07:59 +0900

    feat: NotionDatabase 모델 생성

--- api/models.py
+++ api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class NotionDatabase(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    created_time = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        verbose_name_plural = "Notion databases"
+        db_table = 'notiondb'
+    
+    def __str__(self):
+        return self.title
